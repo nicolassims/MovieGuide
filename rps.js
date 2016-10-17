ffd4882bd157ef4803cd70c7953e1c8db369eff4
nicolassims <sableye.nico@gmail.com>
2016-10-17 15:09:47 -0400

    Added rps.js

---
+++ rps.js
@@ -0,0 +1,47 @@
+/**
+ * @author Sims, Nicolas (sableye.nico@gmail.com)
+ * @version 0.0.1
+ * @summary Design a program for the Hollywood Movie Rating Guide || created: 10.11.2016
+ * @todo
+ */
+
+"use strict";
+const PROMPT = require('readline-sync');
+
+let computerHand, playerHand, victoryMet;
+
+function main() {
+    while (victoryMet == 1 || victoryMet == null) {
+        setComputerHand();
+        setPlayerHand();
+        setVictoryMet();
+    }
+}
+
+main();
+
+function setComputerHand() {
+    computerHand = Math.floor((Math.random() * 3) + 1);
+}
+
+function setPlayerHand() {
+    playerHand = PROMPT.question('<1> = Rock\n<2> = Paper\n<3> = Scissors\n>');
+    while (playerHand != 1 && playerHand != 2 && playerHand != 3) {
+        playerHand = PROMPT.question('That\'s not an option. Select one, two, or three.\n>');
+    }
+}
+
+function setVictoryMet() {
+    const ROCK = 1, PAPER = 2, SCISSORS = 3;
+    if (playerHand == ROCK && computerHand == PAPER || playerHand == PAPER && computerHand == SCISSORS || playerHand ==
+        SCISSORS && computerHand == ROCK) {
+        console.log('Computer wins.');
+        victoryMet = 0;
+    } else if ( playerHand == computerHand) {
+        console.log('Tie. try again.');
+        victoryMet = 1;
+    } else {
+        console.log('You win.');
+        victoryMet = 1;
+    }
+}
